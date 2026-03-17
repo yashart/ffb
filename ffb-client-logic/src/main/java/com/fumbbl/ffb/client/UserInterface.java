7617325f37d7fcff3a27d2761cccafc3c9a46cea
yashart <yashart@mail.ru>
2026-03-17 03:47:57 +0300

    Revert "Merge branch 'wide-vertical-layout' into dev"
    This reverts commit 4313a2385dfce4a8be800d8f5a466aba5e3f31ea, reversing
    changes made to bee883050edf96b041006a26b5741984dcb36c4e.

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/UserInterface.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/UserInterface.java
@@ -139,22 +139,14 @@ public class UserInterface extends JFrame implements WindowListener, IDialogClos
 		switch (layoutSettings.getLayout()) {
 			case PORTRAIT:
 				panelContent = portraitContent();
-                fFieldComponent.disableViewport();
 				break;
 			case SQUARE:
-                panelContent = squareContent();
-                fFieldComponent.enableViewport(16, 16);
+				panelContent = squareContent();
                 //fFieldComponent.enableViewport(8, 8);
 				break;
-            case WIDE:
-                panelContent = landscapeContent();
-                fFieldComponent.disableViewport();
-                //fFieldComponent.enableViewport(16, 16);
-                break;
 			default:
 				panelContent = landscapeContent();
-                fFieldComponent.disableViewport();
-                //fFieldComponent.enableViewport(27, 16);
+                fFieldComponent.enableViewport(16, 10);
 				break;
 		}
 
@@ -226,8 +218,7 @@ public class UserInterface extends JFrame implements WindowListener, IDialogClos
 	private JPanel squareContent() {
 		JPanel fieldPanel = fieldPanel();
 
-
-        JPanel logChatScorePanel = wrapperPanel(BoxLayout.Y_AXIS, getLog(), getScoreBar(), getChat());
+		JPanel logChatScorePanel = wrapperPanel(BoxLayout.Y_AXIS, getLog(), getScoreBar(), getChat());
 		logChatScorePanel.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
 
 		JPanel panelMain = wrapperPanel(BoxLayout.X_AXIS, createSideBarPanel(fSideBarHome), fieldPanel, createSideBarPanel(fSideBarAway));
